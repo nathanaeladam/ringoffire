d212a7af2f7ba15d835675675eb81cc7e0c006d5
Nathanael Adam <nathanaeladam3@gmail.com>
2021-11-07 16:14:46 +0100

    connect

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+   firebasemg: {
+    apiKey: "AIzaSyBK4ujzmstVplGVod3sj5lJWJkCLDGvvyQ",
+    authDomain: "ring-of-fire-2d985.firebaseapp.com",
+    databaseURL: "https://ring-of-fire-2d985-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ring-of-fire-2d985",
+    storageBucket: "ring-of-fire-2d985.appspot.com",
+    messagingSenderId: "619376966444",
+    appId: "1:619376966444:web:7cab2bfc4653e70c640a89"
+  },
 };
 
 /*
