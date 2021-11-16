88f46877708d463c909cdc627e94d6988a676b63
Nathanael Adam <nathanaeladam3@gmail.com>
2021-11-16 17:33:23 +0100

    update the bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import { StartScreenComponent } from './start-screen/start-screen.component';
 import { GameComponent } from './game/game.component';
 import { PlayerComponent } from './player/player.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
 import { AddPlayerDialogComponent } from './add-player-dialog/add-player-dialog.component';
 import { FormsModule } from '@angular/forms';
 import { GameInfoComponent } from './game-info/game-info.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -27,6 +30,8 @@ import { GameInfoComponent } from './game-info/game-info.component';
   ],
   imports: [
     BrowserModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule,
     MatButtonModule,
     MatIconModule,
